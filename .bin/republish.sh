7b4c6584205c45a0f3f6ed80a4380ccab1f49fe4
Thanh huy <adjustmode1@gmail.com>
2024-11-13 09:26:37 +0700

    feat: prepublish

--- .bin/republish.sh
+++ .bin/republish.sh
@@ -3,4 +3,4 @@
 pnpm run build-type
 pnpm run build
 
-find src -mindepth 1 -type d ! -path "src/proto" ! -path "src/typings" -exec rm -rf {} +
+find src -mindepth 1 ! -path "src/proto" ! -path "src/proto/*" ! -path "src/typings" ! -path "src/typings/*" -exec rm -rf {} +
